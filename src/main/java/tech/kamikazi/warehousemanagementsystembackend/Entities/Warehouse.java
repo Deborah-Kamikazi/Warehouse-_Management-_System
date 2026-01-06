26b19eabb2a7392fb13a2569d266577cc46760ae
deborah <deborahkamikazi9@gmail>
2026-01-06 15:32:26 +0200

    creating databese and dtos

---
+++ src/main/java/tech/kamikazi/warehousemanagementsystembackend/Entities/Warehouse.java
@@ -0,0 +1,39 @@
+package tech.kamikazi.warehousemanagementsystembackend.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "warehouse")
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "warehouse_number", nullable = false, unique = true)
+    private String warehouseNumber;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "active" , nullable = false)
+    private boolean active = true;
+
+    @Version
+    @Column(name = "version")
+    private int version;
+
+    @Column(name = "created_time_stamp", updatable = false)
+    private Instant createdTimeStamp;
+
+    @Column(name = "updated_time_stamp")
+    private Instant updatedTimeStamp;
+
+}
---
+++ src/main/java/tech/kamikazi/warehousemanagementsystembackend/dto/WarehouseDto.java
@@ -0,0 +1,19 @@
+package tech.kamikazi.warehousemanagementsystembackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@Data
+public class WarehouseDto {
+  private  long id;
+  private String warehouseNumber;
+  private String name;
+  private boolean active;
+  private int version;
+  private Instant createdTimeStamp;
+  private Instant updatedTimeStamp;
+}
+
