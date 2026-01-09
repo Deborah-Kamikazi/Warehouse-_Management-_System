6ae305f78a0b6651a63db6bcc147b5339ed7e845
deborah <deborahkamikazi9@gmail>
2026-01-09 11:56:38 +0200

    removing some duplication

--- src/main/java/tech/kamikazi/warehousemanagementsystembackend/repositories/LocationRepository.java
+++ src/main/java/tech/kamikazi/warehousemanagementsystembackend/repositories/LocationRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface LocationRepository  extends JpaRepository<Location,Long> {
     boolean existsByWarehouseIdAndLocationCode(int warehouseId, String locationCode);
-    Optional<Location> findByWarehouseIdAndLocationCode(Integer warehouseId, String locationCode);
 
 }
